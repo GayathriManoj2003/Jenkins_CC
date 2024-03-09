dd2b1ab0d95418c950e8a749a7868bbd48db45bb
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2024-03-09 19:35:34 +0530

    Add Jenkins Pipeline Script

---
+++ main.cpp
@@ -0,0 +1,10 @@
+//File: main.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, Jenkins" << endl;
+  cout << "This is main.cpp made for CC Assignment 4 - Task 5" << endl;
+  cout << "Jenkins Pipeline" << endl;
+  return 0;
+}
